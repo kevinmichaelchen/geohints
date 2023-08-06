7cd0d9324d33694b3033cb0522c018b855867499
Kevin Chen <kevinmichaelchen@gmail.com>
2023-08-05 21:45:58 -0400

    feat: styles

--- src/routes/follow/index.tsx
+++ src/routes/follow/index.tsx
@@ -11,21 +11,24 @@ const countries: Country[] = [
     name: "Kenya",
     numImages: 5,
   },
+  {
+    name: "Nigeria",
+    numImages: 16,
+  },
 ];
 
 export default component$(() => {
   return (
-    <div>
+    <div class="space-y-20">
       {countries.map((country, i) => (
         <div key={i}>
           <div class="text-3xl">{country.name}</div>
-          <div class="grid grid-cols-3 gap-y-5">
+          <div class="grid gap-2 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-3 md:gap-5 lg:gap-10">
             {new Array(country.numImages).fill(0).map((_, i) => (
               <div key={i}>
                 <Image
-                  src={`/images/follow/${country.name.toLowerCase()}${
-                    i + 1
-                  }.webp`}
+                  src={`/images/follow/${country.name.toLowerCase()}${i + 1
+                    }.webp`}
                   layout="constrained"
                   width={400}
                   height={250}
