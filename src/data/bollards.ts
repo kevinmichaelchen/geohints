08e85cc78ef0e135d1f153d9f111f64d2c294810
Kevin Chen <kevinmichaelchen@gmail.com>
2025-12-29 12:07:18 -0500

    feat(geo-f7e): add bollards page with R2 images
    Integrate scraped bollard images from R2 into the site:
    - Add generate-bollards-data.ts script to create TypeScript data from manifest
    - Generate src/data/bollards.ts with 88 countries, 154 images
    - Create bollards route with continent filtering and responsive srcset

---
+++ src/data/bollards.ts
@@ -0,0 +1,816 @@
+/**
+ * Bollard data generated from scraped images
+ * Generated: 2025-12-29T17:06:13.533Z
+ * Total: 88 countries, 154 images
+ */
+
+export const R2_BASE_URL = 'https://pub-3d7bacd76def438caae68643612e60f9.r2.dev';
+
+export interface BollardCountry {
+  /** ISO 3166-1 alpha-2 country code (uppercase) */
+  code: string;
+  /** Country name */
+  name: string;
+  /** Continent */
+  continent: string;
+  /** Image sequence numbers */
+  images: number[];
+}
+
+export const bollardCountries: BollardCountry[] = [
+  {
+    "code": "AL",
+    "name": "Albania",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "AD",
+    "name": "Andorra",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "AR",
+    "name": "Argentina",
+    "continent": "South America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "AU",
+    "name": "Australia",
+    "continent": "Oceania",
+    "images": [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+    ]
+  },
+  {
+    "code": "AT",
+    "name": "Austria",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+    ]
+  },
+  {
+    "code": "BD",
+    "name": "Bangladesh",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "BE",
+    "name": "Belgium",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "BM",
+    "name": "Bermuda",
+    "continent": "North America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "BO",
+    "name": "Bolivia",
+    "continent": "South America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "BW",
+    "name": "Botswana",
+    "continent": "Africa",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "BR",
+    "name": "Brazil",
+    "continent": "South America",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "BG",
+    "name": "Bulgaria",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "KH",
+    "name": "Cambodia",
+    "continent": "Asia",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "CA",
+    "name": "Canada",
+    "continent": "North America",
+    "images": [
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
+  },
+  {
+    "code": "CL",
+    "name": "Chile",
+    "continent": "South America",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "CX",
+    "name": "Christmas Island",
+    "continent": "Oceania",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "CO",
+    "name": "Colombia",
+    "continent": "South America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "HR",
+    "name": "Croatia",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "CZ",
+    "name": "Czechia",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "DK",
+    "name": "Denmark",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "DO",
+    "name": "Dominican Republic",
+    "continent": "North America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "EC",
+    "name": "Ecuador",
+    "continent": "South America",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "EE",
+    "name": "Estonia",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "SZ",
+    "name": "Eswatini",
+    "continent": "Africa",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "FO",
+    "name": "Faroe Islands",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "FI",
+    "name": "Finland",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "FR",
+    "name": "France",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "DE",
+    "name": "Germany",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "GH",
+    "name": "Ghana",
+    "continent": "Africa",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "GR",
+    "name": "Greece",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "GL",
+    "name": "Greenland",
+    "continent": "North America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "GU",
+    "name": "Guam",
+    "continent": "Oceania",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "GT",
+    "name": "Guatemala",
+    "continent": "North America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "HU",
+    "name": "Hungary",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "IS",
+    "name": "Iceland",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "IN",
+    "name": "India",
+    "continent": "Asia",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "ID",
+    "name": "Indonesia",
+    "continent": "Asia",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "IE",
+    "name": "Ireland",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "IM",
+    "name": "Isle of Man",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "IL",
+    "name": "Israel",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "IT",
+    "name": "Italy",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "JP",
+    "name": "Japan",
+    "continent": "Asia",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "JE",
+    "name": "Jersey",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "KE",
+    "name": "Kenya",
+    "continent": "Africa",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "KG",
+    "name": "Kyrgyzstan",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "LA",
+    "name": "Laos",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "LV",
+    "name": "Latvia",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "LS",
+    "name": "Lesotho",
+    "continent": "Africa",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "LT",
+    "name": "Lithuania",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "LU",
+    "name": "Luxembourg",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "MY",
+    "name": "Malaysia",
+    "continent": "Asia",
+    "images": [
+      1,
+      2,
+      3,
+      4
+    ]
+  },
+  {
+    "code": "MT",
+    "name": "Malta",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "MX",
+    "name": "Mexico",
+    "continent": "North America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "MN",
+    "name": "Mongolia",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "ME",
+    "name": "Montenegro",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "NL",
+    "name": "Netherlands",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "NZ",
+    "name": "New Zealand",
+    "continent": "Oceania",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "MK",
+    "name": "North Macedonia",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3,
+      4
+    ]
+  },
+  {
+    "code": "NO",
+    "name": "Norway",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "PE",
+    "name": "Peru",
+    "continent": "South America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "PH",
+    "name": "Philippines",
+    "continent": "Asia",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "PL",
+    "name": "Poland",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "PT",
+    "name": "Portugal",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "PR",
+    "name": "Puerto Rico",
+    "continent": "North America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "RO",
+    "name": "Romania",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "RU",
+    "name": "Russia",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "SM",
+    "name": "San Marino",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "SN",
+    "name": "Senegal",
+    "continent": "Africa",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "RS",
+    "name": "Serbia",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "SG",
+    "name": "Singapore",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "SK",
+    "name": "Slovakia",
+    "continent": "Europe",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "SI",
+    "name": "Slovenia",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "ZA",
+    "name": "South Africa",
+    "continent": "Africa",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "KR",
+    "name": "South Korea",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "ES",
+    "name": "Spain",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "LK",
+    "name": "Sri Lanka",
+    "continent": "Asia",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "SE",
+    "name": "Sweden",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
+  },
+  {
+    "code": "CH",
+    "name": "Switzerland",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "TW",
+    "name": "Taiwan",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "TH",
+    "name": "Thailand",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "TN",
+    "name": "Tunisia",
+    "continent": "Africa",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "TR",
+    "name": "Turkey",
+    "continent": "Asia",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "UA",
+    "name": "Ukraine",
+    "continent": "Europe",
+    "images": [
+      1,
+      2
+    ]
+  },
+  {
+    "code": "AE",
+    "name": "United Arab Emirates",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "GB",
+    "name": "United Kingdom",
+    "continent": "Europe",
+    "images": [
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "code": "US",
+    "name": "United States",
+    "continent": "North America",
+    "images": [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6
+    ]
+  },
+  {
+    "code": "UY",
+    "name": "Uruguay",
+    "continent": "South America",
+    "images": [
+      1
+    ]
+  },
+  {
+    "code": "VN",
+    "name": "Vietnam",
+    "continent": "Asia",
+    "images": [
+      1
+    ]
+  }
+];
+
+export const continents = [...new Set(bollardCountries.map(c => c.continent))].sort();
+
+/**
+ * Get R2 URL for a bollard image
+ */
+export function getBollardImageUrl(
+  countryCode: string,
+  sequence: number,
+  size: 400 | 800 | 1200 = 800
+): string {
+  const cc = countryCode.toLowerCase();
+  const seq = String(sequence).padStart(3, '0');
+  return `${R2_BASE_URL}/bollards/${cc}/${cc}-${seq}-${size}w.webp`;
+}
+
+/**
+ * Get srcset for a bollard image
+ */
+export function getBollardSrcset(countryCode: string, sequence: number): string {
+  const cc = countryCode.toLowerCase();
+  const seq = String(sequence).padStart(3, '0');
+  const base = `${R2_BASE_URL}/bollards/${cc}/${cc}-${seq}`;
+  return `${base}-400w.webp 400w, ${base}-800w.webp 800w, ${base}-1200w.webp 1200w`;
+}
